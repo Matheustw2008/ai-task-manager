c159f993f0e13f30521f8297e8f638fca2c03889
Matheustw2008 <mh1460400@gmail.com>
2026-04-01 10:06:00 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8001";
+const URL_BASE = import.meta.env.VITE_API_URL || "http://127.0.0.1:8001";
 
 const api = axios.create({
   baseURL: BASE_URL,
